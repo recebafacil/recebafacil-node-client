89f92230f2bfdd46e2c53b0f57dac2305db9d4e9
Edinei Rodrigues Filho <edineibk@gmail.com>
2021-09-08 16:42:03 -0300

    fix(feature/buyer-service): Correção da API que consume a API de serviços de compradores.

--- src/dto/BuyerDTO.ts
+++ src/dto/BuyerDTO.ts
@@ -1,22 +1,28 @@
 import { BuyerInterface } from '../interfaces';
+import { RequiredBy } from '../types';
 
-export type CreateBuyerDTO = Pick<
-  BuyerInterface,
-  | 'name'
-  | 'document_number'
-  | 'email'
-  | 'email_secondary'
-  | 'phone_number'
-  | 'birthdate'
-  | 'obs'
-  | 'street'
-  | 'house_number'
-  | 'postal_code'
-  | 'neighborhood'
-  | 'city'
-  | 'state_initials'
-  | 'state'
-  | 'country_code'
+export type CreateBuyerDTO = RequiredBy<
+  Partial<
+    Pick<
+      BuyerInterface,
+      | 'name'
+      | 'document_number'
+      | 'email'
+      | 'email_secondary'
+      | 'phone_number'
+      | 'birthdate'
+      | 'obs'
+      | 'street'
+      | 'house_number'
+      | 'postal_code'
+      | 'neighborhood'
+      | 'city'
+      | 'state_initials'
+      | 'state'
+      | 'country_code'
+    >
+  >,
+  'name' | 'document_number' | 'email'
 >;
 
 export type UpdateBuyerDTO = Partial<
